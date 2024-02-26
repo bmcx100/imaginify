8cf817010d811b0b248a5fac50847c2124b751b2
Ryan Hurst <bmcx100@gmail.com>
2024-02-26 12:21:01 -0500

    implemented photo changes

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -9,10 +9,12 @@ import React from 'react'
 //   )
 // }
 
-const Header = ( ) => {
+const Header = ( {title, subtitle} : {title: string, subtitle?: string } ) => {
   return (
     <>
-      <h1>Header</h1>
+      <h2 className="h2-bold text-dark-600">{title}</h2>
+      {subtitle && <p className="p-16-regular mt-4">
+        {subtitle}</p>}
     </>
   )
 }
